292f7937bf4c8f136ad849f1415079e45c589249
Niraj Kamdar <nirajkamdar27@pm.me>
2020-10-08 20:01:02 +0530

    add tempfile module

---
+++ async_files/tempfile.py
@@ -0,0 +1,28 @@
+import tempfile
+from typing import Callable, Awaitable
+
+from . import FileIO, async_wraps
+
+
+class TemporaryFile(FileIO):
+    OPEN = tempfile.TemporaryFile
+
+
+class NamedTemporaryFile(FileIO):
+    OPEN = tempfile.NamedTemporaryFile
+
+
+class SpooledTemporaryFile(FileIO):
+    OPEN = tempfile.SpooledTemporaryFile
+
+
+class mkstemp(FileIO):
+    OPEN = tempfile.mkstemp
+
+
+mkdtemp: Callable[[], Awaitable[str]] = async_wraps(tempfile.mkdtemp)
+gettempdir: Callable[[], Awaitable[str]] = async_wraps(tempfile.gettempdir)
+gettempdirb: Callable[[], Awaitable[bytes]] = async_wraps(tempfile.gettempdirb)
+gettempprefix: Callable[[], str] = tempfile.gettempprefix
+gettempprefixb: Callable[[], bytes] = tempfile.gettempprefixb
+tempdir: str = tempfile.tempdir
