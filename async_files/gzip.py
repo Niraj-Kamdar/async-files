9203b8089be47f6faa5fb40028591bc10130fe7e
Niraj Kamdar <51387861+Niraj-Kamdar@users.noreply.github.com>
2020-10-12 12:02:40 +0530

    Add gzip specific attributes (#10)

--- async_files/gzip.py
+++ async_files/gzip.py
@@ -3,15 +3,27 @@ from typing import Awaitable
 from typing import Callable
 
 from .fileio import FileIO
+from .fileobj import DEFAULT_CONFIG
+from .fileobj import FileObj
 from .utils import async_wraps
+
+GZIP_CONFIG = DEFAULT_CONFIG
+GZIP_CONFIG["bytes_sync_attrs"].append("mtime")
+
+
+class GzipFileObj(FileObj):
+    CONFIG = GZIP_CONFIG
+    mtime: int
 
 
 class open(FileIO):
     OPEN = gzip.open
+    FILEOBJ = GzipFileObj
 
 
 class GzipFile(FileIO):
     OPEN = gzip.GzipFile
+    FILEOBJ = GzipFileObj
 
 
 compress: Callable[[], Awaitable[bytes]] = async_wraps(gzip.compress)
